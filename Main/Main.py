054afb7d9b2ee038cb48fc7d8713d34d6121c4d3
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-03-11 15:25:25 +0100

    Update Main.py

--- Main/Main.py
+++ Main/Main.py
@@ -523,7 +523,7 @@ if app_mode == '🏠 Inicio':
 
     st.write('#### 📈 Análisis de la visibilidad otorgada por deporte, género del redactor, equipos de fútbol, etc. y su repercusión (en web + twitter) de las noticias de las primeras planas digitales de los principales diarios deportivos en España. El objetivo de este estudio es, a partir de los datos, poner de manifiesto si existen sesgos en las decisiones de los propios diarios deportivos a la hora de decidir a qué dar visibilidad en materia deportiva.')
 
-    st.write('##### 🔎 Puedes navegar a través de diferentes gráficos interactivos usando el panel de la izquierda, confeccionando tu propio gráfico según campos, ejes y métricas que desees analizar.')
+    st.write('##### 🔎 Puedes navegar a través de diferentes gráficos interactivos usando el panel de la izquierda, confeccionando tu propio gráfico según campos, ejes y métricas que desees analizar. Los datos corresponden a las noticias del mes de enero de 2023.')
 	
     st.markdown(
         """
