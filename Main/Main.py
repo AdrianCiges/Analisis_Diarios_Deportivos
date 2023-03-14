d3b9b9e42893cc345cdcca24e30bad4f02b2591b
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-03-14 11:38:21 +0100

    Update Main.py

--- Main/Main.py
+++ Main/Main.py
@@ -161,7 +161,7 @@ def heatmap(x,y,z=0):
     
     fig.update_layout(height=500, yaxis=dict(categoryorder='category descending'))
     fig.update_layout(
-    title={'text': f"{metrica.capitalize()} por {app_mode.upper()} y {ejey.upper()}",'font_size': 24, 'xanchor': 'center'},
+    title={'text': f"{metrica.capitalize()} por {app_mode.upper()} y {ejey.upper()}",'font_size': 24},
     xaxis_title=f'<b style="font-size:1.2em">{x}</b>',
     yaxis_title=f'<b style="font-size:1.4em">{ejey}</b>',
     #legend_title=f'<b style="font-size:1.6em">{ejey}</b>',
