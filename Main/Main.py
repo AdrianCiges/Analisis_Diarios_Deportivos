223740e825b720e74805325db710bbab56c29a36
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-03-14 12:10:33 +0100

    Update Main.py

--- Main/Main.py
+++ Main/Main.py
@@ -469,7 +469,7 @@ def treemap(x,y,z=0):
     fig.update_traces(root_color="lightgrey")
 
     fig.update_layout(
-        title={'text': f"Proporción de {metrica} por {app_mode.upper()} y {ejey.upper()}",'font_size': 24},
+        title={'text': f"Proporción de {metrica} por {app_mode.upper()} y {ejey.upper()}",'font_size': 24,'x':0.5,'y':0.95,'xanchor': 'center','yanchor': 'top'},
         xaxis_title=f'<b style="font-size:1.2em">{x}</b>',
         yaxis_title=f'<b style="font-size:1.4em">nº de noticias</b>',
         legend_title=f'<b style="font-size:1.6em">{y}</b>',
