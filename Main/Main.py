a602ec71b6e8f76264522be286e5d7d86b96b1ed
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-03-16 06:44:10 +0100

    Update Main.py
    Correcciones texto

--- Main/Main.py
+++ Main/Main.py
@@ -548,7 +548,7 @@ def sol(x,y,z=0):
     )
     return fig
 
-app_mode = st.sidebar.selectbox('Visibilidad por (_elegir **eje X**_):',['🏠 Inicio', '💻 Web','🏊🏻 Deporte','⚽ Equipo','🚻 Género redactor/a'])
+app_mode = st.sidebar.selectbox('Visibilidad por (_elegir **eje horizontal**_):',['🏠 Inicio', '💻 Web','🏊🏻 Deporte','⚽ Equipo','🚻 Género redactor/a'])
 
 if app_mode == '🏠 Inicio':
 
@@ -570,11 +570,11 @@ if app_mode == '🏠 Inicio':
 
     st.write('#### 🔎 Cómo crear tus gráficos')
     with st.expander('_La diversión empieza aquí_', expanded=True): 
-        st.write('⬅️ Utiliza el panel de la izquierda para elegir filtrar datos y elegir qué ver en los ejes, colores, etc.')
-        st.write('🎯 En "**Filtrar Datos**" puedes elegir **QUÉ VER**, filtrando por DIMENSIONES [_por ejemplo noticias de fútbol (seccion) en SuperDeporte y AS (web), etc._] y/o por MÉTRICAS (los valores numéricos) [_por ejemplo, noticias con más de 10 RT y menos de 20 LIKES, etc._]')
-        st.write('👀 En los **desplegables** inferiores puedes elegir **CÓMO VERLO**, eligiendo qué ver en el **eje horizontal** usando el **primer desplegable**, y por qué dimensión **desagrupar por color** usando el **segundo desplgable**')
-        st.write('❗ Si eliges **Repercusión** en el **segundo desplegable**, podrás elegir qué **MÉTRICA** ver en el **eje vertical** usando el **cuarto desplegable** y, nuevamente, por qué por qué dimensión **desagrupar por color** usando el **tercer desplegable**')
-        st.write('🌐 Si no especificas algún filtro, se seleccionan todos las DIMENSIONES y MÉTRICAS')
+        st.write('⬅️ Utiliza el **panel de la izquierda** para elegir filtrar datos y decidir qué ver en los ejes, en los colores, etc.')
+        st.write('🎯 En "**Filtrar Datos**" puedes elegir **QUÉ VER**, filtrando por DIMENSIONES [_por ejemplo noticias de baloncesto y motor (seccion) publicadas en SuperDeporte y AS (web), etc._] y/o por MÉTRICAS (los valores numéricos) [_por ejemplo, noticias con más de 10 RT y menos de 20 LIKES, etc._]')
+        st.write('👀 En los **desplegables** de la parte inferior del menú lateral puedes elegir **CÓMO VERLO**, eligiendo qué ver en el **eje horizontal** usando el **primer desplegable**, y por qué dimensión **desagrupar por color** usando el **segundo desplgable**')
+        st.write('❗ Si eliges **Repercusion** en el **segundo desplegable**, podrás elegir qué **MÉTRICA** ver en el **eje vertical** usando el **cuarto desplegable** y, nuevamente, por qué dimensión **desagrupar por color** usando el **tercer desplegable**')
+        st.write('🌐 Si no especificas algún filtro, se seleccionan todos las DIMENSIONES y MÉTRICAS de los datos')
 
 
 
